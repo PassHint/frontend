fff638014605b8cdf8afbddcdcde84ab5ff4cbd1
STL-FelipeGomes <felipegomesoliveira12@gmail.com>
2024-05-26 19:59:46 -0300

    :construction: construction page hit

--- src/pages/Hit/Hit.tsx
+++ src/pages/Hit/Hit.tsx
@@ -1,10 +1,46 @@
-import { Box } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from '@chakra-ui/react';
+import { AddIcon, SearchIcon } from '@chakra-ui/icons';
 import { TemplateScreen } from '../../components/TemplateScreen/TemplateScreen';
 
 export default function Hit() {
   return (
     <TemplateScreen>
-      <Box>asdasdas</Box>
+      <Box
+        boxShadow='xs'
+        border='1px solid'
+        borderColor='cyanX.100'
+        p='6'
+        rounded='md'
+        textColor='whiteX.100'
+      >
+        <Flex gap='1rem'>
+          <InputGroup>
+            <InputLeftAddon background='transparent' padding='.5rem'>
+              <SearchIcon />
+            </InputLeftAddon>
+            <Input
+              type='text'
+              placeholder='Buscar pelo site'
+              _focus={{ boxShadow: 'none', borderColor: 'cyanX.100' }}
+            />
+          </InputGroup>
+          <Button
+            background='transparent'
+            padding='0'
+            _hover={{ background: 'cyanX.100' }}
+            _focus={{ background: 'cyanX.100' }}
+          >
+            <AddIcon textColor='whiteX.100' />
+          </Button>
+        </Flex>
+      </Box>
     </TemplateScreen>
   );
 }
